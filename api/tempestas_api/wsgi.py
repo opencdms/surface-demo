ce121f160f7ddf04113e13d588b9a78978094509
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-09 02:18:41 +0600

    optional change in django settings module

--- api/tempestas_api/wsgi.py
+++ api/tempestas_api/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tempestas_api.settings")
+if not os.getenv("OPENCDMS_API"):
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tempestas_api.settings")
+else:
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "surface.api.tempestas_api.settings")
 
 application = get_wsgi_application()
