6182c5d996220fcedd9fc5196dc1b196d22add9b
Faysal Ishtiaq Rabby <f.i.rabby@gmail.com>
2021-12-08 20:18:43 +0600

    tweak for opencdms api

--- api/tempestas_api/__init__.py
+++ api/tempestas_api/__init__.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import, unicode_literals
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+import os
 
-__all__ = ('celery_app',)
+if not os.getenv("OPENCDMS_API"):
+    from .celery import app as celery_app
+
+    __all__ = ('celery_app',)
